702b9ebdd796d1fc865649c78c412f331129626e
spacesoldier <kibersoldier@gmail.com>
2015-10-20 03:13:40 +0300

    fixed a problem with utf8 collations in 'RegAuthor' stored procedure. the sql exception output in servlet is turned off. TODO: use log4j in future

--- QuestReactorJavaBackend/src/com/soloway/gaming/social/quests/server/RegAuthorServlet.java
+++ QuestReactorJavaBackend/src/com/soloway/gaming/social/quests/server/RegAuthorServlet.java
@@ -74,9 +74,9 @@ public class RegAuthorServlet extends HttpServlet {
 				
 				
 			} catch(SQLException e){
-				regResult.setStatusCode(-2);
-				regResult.setErrorMessage("Error: "+e.getMessage());
-//				e.printStackTrace();
+				//regResult.setStatusCode(-2);
+				//regResult.setErrorMessage("Error: "+e.getMessage());
+				e.printStackTrace();
 			}
 			
 			
--- db/reg_author.sql
+++ db/reg_author.sql
@@ -3,14 +3,14 @@ CREATE DEFINER=`root`@`%` PROCEDURE `RegAuthor`(IN new_email VARCHAR(255), OUT s
 BEGIN
 	DECLARE tmp_uuid varchar(255);
 	
-	if (select exists (select * from quest_db.emails where email = new_email)) then
+	if (select exists (select * from quest_db.emails where email = new_email COLLATE utf8_unicode_ci)) then
 		set statuscode = -100;
-		set statusmessage = "Email already exists";
+		set statusmessage = 'Email already exists';
 	else
 		set tmp_uuid = uuid();
-		insert into quest_db.emails (email,verify_code,status) values (new_email,tmp_uuid,"non-verified");
+		insert into quest_db.emails (email,verify_code,userstatus) values (new_email COLLATE utf8_unicode_ci,tmp_uuid,'non-verified');
 		set statuscode = 0;
-		set statusmessage = "OK";
+		set statusmessage = 'OK';
 	end if;
 END$$
 DELIMITER ;
